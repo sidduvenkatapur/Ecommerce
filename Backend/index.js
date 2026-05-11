b3b4b4d6589b10dbbadfabf7ee84067c36b63576
Siddalingappa Venkatapur <57409661+sidduvenkatapur@users.noreply.github.com>
2026-05-11 23:29:36 +0530

    Update image URL to use environment variable

--- Backend/index.js
+++ Backend/index.js
@@ -60,7 +60,7 @@ app.use('/images',express.static('upload/images'))
 app.post('/upload',upload.single('product'),(req,res)=>{
  res.json({
     success:1,
-    image_url:`http://localhost:${port}/images/${req.file.filename}`
+    image_url:`${process.env.REACT_APP_API_URL}/images/${req.file.filename}`
  })
 })
 
