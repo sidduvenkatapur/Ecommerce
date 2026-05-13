4b6bbc5f83c0039edb543f95253f04e7fcb6d812
Siddalingappa Venkatapur <57409661+sidduvenkatapur@users.noreply.github.com>
2026-05-13 13:53:32 +0530

    Update API URLs to use environment variable

--- my-app/src/components/pages/Signup.js
+++ my-app/src/components/pages/Signup.js
@@ -18,7 +18,7 @@ const handleChange = (e)=>{
 // },[])
 
 const handleSignup = async()=>{
-  const responsedata = await fetch("http://localhost:4000/signup",{
+  const responsedata = await fetch(`${process.env.REACT_APP_API_URL}/signup`,{
     method:"POST",
     headers:{
         Accept:"application/form-data",
@@ -39,7 +39,7 @@ const handleSignup = async()=>{
 }
 
 const handleLogin = async()=>{
-  const responsedata = await fetch("http://localhost:4000/login",{
+  const responsedata = await fetch(`${process.env.REACT_APP_API_URL}/login`,{
     method:"POST",
     headers:{
         Accept:"application/form-data",
