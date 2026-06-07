14424555134f60a1d5f05948f8b2e5ea9b4787a0
sidduvenkatapur <sidduvenkatapur@gmail.com>
2026-06-07 20:51:46 +0530

    secretes changes

--- Backend/index.js
+++ Backend/index.js
@@ -51,8 +51,8 @@ app.listen(port,(error)=>{
 const s3 = new S3Client({
     region:'us-east-1',
     credentials:{
-        accessKeyId:process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey:process.env.AWS_SECRET_ACCESS_KEY,
+        accessKeyId:secrets.AWS_ACCESS_KEY_ID,
+        secretAccessKey:secrets.AWS_SECRET_ACCESS_KEY,
     }
 });
 
