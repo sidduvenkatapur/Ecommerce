7c2b2a7836e2361ef1c3fd5cf415a99a9fbe12f0
sidduvenkatapur <sidduvenkatapur@gmail.com>
2026-06-08 12:35:51 +0530

    images changes

--- my-app/src/components/Items/Item.js
+++ my-app/src/components/Items/Item.js
@@ -4,24 +4,28 @@ import { Link } from 'react-router-dom'
 
 const Item = ({data}) => {
 
-//   const getImageSrc = () => {
-//     if (!data || !data.image) return "";
+const getImageSrc = () => {
+    if (!data || !data.image) return "";
 
-//     if (/^https?:\/\//i.test(data.image)) {
-//         return data.image;
-//     }
+    // 1. If it's a new upload (already contains http:// or https://), use it natively
+    if (data.image.startsWith('http')) {
+        return data.image;
+    }
 
-//     // const baseUrl = (process.env.REACT_APP_API_URL || 'http://app-load-balancer-2109406327.us-east-1.elb.amazonaws.com:8080').replace(/\/$/, '');
-//     const baseUrl = "https://ecommerce-product-images-s3-865230234414-us-east-1-an.s3.us-east-1.amazonaws.com"
-//     const relativePath = data.image.replace(/^\/+/, '');
-//     return `${baseUrl}/${relativePath}`;
-// };
+    // 2. Your live AWS backend container URL where your old images live on disk
+    const backendUrl = process.env.REACT_APP_API_URL
+    // 3. Extract just the raw filename to strip out any duplicate folder prefixes
+    const fileName = data.image.split('/').pop();
+
+    // 4. Combine them cleanly to point directly to your backend's static file folder
+    return `${backendUrl}/images/${fileName}`;
+};
 
   return (
     <Link to={`/product/${data.id}`}>
     <div className='flex flex-col'>
-     <img alt="item" src={data.image.startsWith('http') ? data.image : `${process.env.REACT_APP_API_URL}/${data.image}`} />
-    {/* <img alt="item" src={getImageSrc()} /> */}
+     {/* <img alt="item" src={data.image.startsWith('http') ? data.image : `${process.env.REACT_APP_API_URL}/${data.image}`} /> */}
+    <img alt="item" src={getImageSrc()} />
     <p className='py-4 text-lg'>{data.name}</p>
     <p className=''><span className='pr-4'>${data.new_price}</span> <span className='text text-neutral-400 line-through'>${data.old_price}</span></p>
     </div>
